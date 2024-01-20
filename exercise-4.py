36cd6ceae6e359b169d6d5d14dfb051714618037
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-21 00:34:25 +0800

    Def function

--- exercise-4.py
+++ exercise-4.py
@@ -1,6 +1,7 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
 # Def function
+def remove_chars(string, n):
 
 # Check if n is less than the length of the string
 
