2ac4c5c5b44fecce51968eb05b66f38f60e770c3
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-20 23:52:21 +0800

    Pseudocode

---
+++ exercise-4.py
@@ -0,0 +1,13 @@
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# Def function
+
+# Check if n is less than the length of the string
+
+# Use string slicing
+
+# Define original string
+
+# Display result
+
+# Outputs
